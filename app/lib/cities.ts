466e26bcdeeb562daf2c63046a0530c1e2e0a61d
Tr33horn <86814202+Tr33horn@users.noreply.github.com>
2026-01-09 19:23:15 -0800

    Update cities.ts

--- app/lib/cities.ts
+++ app/lib/cities.ts
@@ -172,6 +172,7 @@ export const CITIES: City[] = [
   { name: "Snoqualmie, WA", lat: 47.5287, lon: -121.8254 },
   { name: "Vashon, WA", lat: 47.4484, lon: -122.4590 },
   { name: "Jonny Seamans Lair", lat: 47.4239, lon: -122.4314 },
+  { name: "Vanilla Lane", lat: 47.4482, lon: -122.4563 },
 
   // West Virginia
   { name: "Charleston, WV", lat: 38.3498, lon: -81.6326 },
