68e78ba5675e6d0643eaa576dda41ded6ef9be01
Tr33horn <86814202+Tr33horn@users.noreply.github.com>
2026-01-23 07:39:17 -0800

    AUTH 6

--- app/(app)/start/_components/StartSignIn.tsx
+++ app/(app)/start/_components/StartSignIn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 import { sanitizeNext } from "@/app/lib/authRedirect";
